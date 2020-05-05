fe40b705e693d31ea7165b0bc53d7799c5d0632e
Sunny Singha <sunnysingha911@gmail.com>
2020-05-06 05:05:29 +0530

    Updates

---
+++ precache-manifest.4d8f46ee14225d62444439c1f58cbd05.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb0fc3a85ae41f5f76a7cc7fd83fc186",
+    "url": "/React-app/index.html"
+  },
+  {
+    "revision": "042095e399aac6d6d1b5",
+    "url": "/React-app/static/css/main.2d99788c.chunk.css"
+  },
+  {
+    "revision": "13dc4b444ff612b9cfe4",
+    "url": "/React-app/static/js/2.f64dd4af.chunk.js"
+  },
+  {
+    "revision": "ef7f0128066dad638e14e7838f835a83",
+    "url": "/React-app/static/js/2.f64dd4af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "042095e399aac6d6d1b5",
+    "url": "/React-app/static/js/main.d17a5bdd.chunk.js"
+  },
+  {
+    "revision": "d5f053a5f67427609c13",
+    "url": "/React-app/static/js/runtime-main.640f74ac.js"
+  },
+  {
+    "revision": "ae4481032cf60e354cb5385a1e434311",
+    "url": "/React-app/static/media/headbg.ae448103.jpg"
+  },
+  {
+    "revision": "d9d79795e8a9c827ffbb5ff16f4baa39",
+    "url": "/React-app/static/media/side.d9d79795.png"
+  },
+  {
+    "revision": "29d52323ea03b4c90c5ce903f4942b6a",
+    "url": "/React-app/static/media/side2.29d52323.png"
+  },
+  {
+    "revision": "32f56a1087679bf1cf0882c3d5124a9e",
+    "url": "/React-app/static/media/side3.32f56a10.png"
+  }
+]);
